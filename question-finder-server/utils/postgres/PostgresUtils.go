1026a69b143ae327d6b3170ec2d5a446e74c52d6
Ratik Gambhir <ratikgambhir@Mac.ht.home>
2025-09-05 13:58:35 -0500

    removed commented code

--- question-finder-server/utils/postgres/PostgresUtils.go
+++ question-finder-server/utils/postgres/PostgresUtils.go
@@ -8,17 +8,6 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// func ConnectToPostgres() (*pgxpool.Pool, error) {
-// 	var pool *pgxpool.Pool
-// 	var err error
-// 	connStr := "postgres://postgres:RjChicago23!@localhost:5432/postgres?sslmode=disable"
-// 	pool, err = pgxpool.New(context.Background(), connStr)
-// 	if err != nil {
-// 		log.Fatal("Failed to connect to DB:", err)
-// 	}
-// 	return pool, nil
-// }
-
 func ConnectToProdPostgres() (*pgxpool.Pool, error) {
 	var pool *pgxpool.Pool
 	var err error

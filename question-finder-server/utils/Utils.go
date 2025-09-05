5085d5a624a9cc743b1c8fbf7e517ba48ffd8d88
Ratik Gambhir <ratikgambhir@Mac.ht.home>
2025-09-05 18:11:30 -0500

    added env function and removed unused struct

--- question-finder-server/utils/Utils.go
+++ question-finder-server/utils/Utils.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"errors"
+	"os"
+)
+
+func getEnvVariable(key string) (string, error) {
+	env := os.Getenv(key)
+	if env != "" {
+		return env, nil
+	}
+	return "", errors.New("env variable not set")
+}
